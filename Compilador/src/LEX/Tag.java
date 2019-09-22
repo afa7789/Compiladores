5857c6dc35e28c361e288dee7e7ccb46932a420d
abeilice <afa7789@gmail.com>
2019-09-22 15:15:59 -0300

    upando pasta do projeto

---
+++ Compilador/src/LEX/Tag.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LEX;
+
+/**
+ *
+ * @author aluno-ccc
+ */
+public class Tag {
+    
+    public final static String 
+            // palavras reservadas
+            EXIT            = "EXIT",
+            START           = "START" ,
+            // tipos
+            INT             = "INT",
+            FLOAT           = "FLOAT",
+            STRING          = "STRING",
+            // controle de fluxo
+            IF              = "IF",
+            THEN            = "THEN",
+            ELSE            = "ELSE",
+            END             = "END",
+            // repeticao
+            DO              = "DO",
+            WHILE           = "WHILE",
+            // IO
+            SCAN            = "SCAN",
+            PRINT           = "PRINT",
+            // booleanos
+            NOT             = "NOT",
+            OR              = "OR",
+            AND             = "AND",
+            
+            // aspas
+            // “
+            QUOTE_RIGHT     = "“",
+            
+            // ”
+            QUOTE_LEFT      = "”",
+            
+            //operadores
+            // +
+            SUM             = "SUM",
+            
+            // -
+            MINUS           = "MINUS",
+            // * 
+            MULT            = "MULT",
+            
+            // /
+            DIV             = "DIV",
+            
+            // ==
+            COMPARATION     = "COMPARATION",
+            
+            // =
+            EQUAL           = "EQUAL",
+            
+            // >
+            GREATHER_THAN   = "GREATHER_THAN",
+            
+            // <
+            LESS_THAN       = "LESS_THAN",
+            
+            // >=
+            GREATHER_EQUAL  = "GREATHER_EQUAL",
+            
+            // <=
+            LESS_EQUAL      = "LESS_EQUAL",
+            
+            // <>
+            DIFF            = "DIFF",
+            
+            // ;
+            SEMICOLON       = "SEMICOLON",
+            
+            // ,
+            COMMA           = "COMMA",
+            
+            // (  )
+            OPEN_PAR        = "OPEN_PAR",
+            
+            CLOSE_PAR       = "CLOSE_PAR",
+            
+            // {  }
+            OPEN_C          = "OPEN_C",
+            CLOSE_C         = "CLOSE_C",
+            
+            // '.'
+            DOT             = "DOT",
+            
+            // contante literal            
+            LITERAL         = "LITERAL",
+            
+            // contantes numericas
+            INTEGER         = "INTEGER",
+            FLOATING        = "FLOATING",
+            
+            // identificadores
+            ID              = "ID",
+            
+            // token desconhecido
+            UNKNOWN         = "UNKNOWN",
+            COMMENT         = "COMMENT";
+
+}
---
+++ Compilador/src/compilador/Compilador.java
@@ -0,0 +1,14 @@
+package compilador;
+
+public class Compilador {
+
+    public static void main(String[] args) {
+        // recebe uma lista com comandos em texto. tem 1 arquivo de texto entrado pelo args.
+        //1 ª etapa. precisa identificar os tokens, colocar eles em tabelas de simbolos.
+        // mostra os tokens 
+        
+        
+        
+    }
+    
+}
